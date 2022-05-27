e6525b4a577c6b66354f736432f97cc5f14f414f
farhana17 <nuho.mohaimin2017@gmail.com>
2022-05-26 21:58:42 -0500

    fdaffsf

---
+++ src/Example/javaexample.java
@@ -0,0 +1,28 @@
+package Example;
+
+public class javaexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+	int a = 30;
+	int b = 40;
+	int c = a+b;
+	
+	System.out.println(c);
+		
+	String firstname ="Farhana";
+	String lastname = " Rahman";
+	String fullname = firstname + lastname;
+	
+	System.out.println(fullname);
+	
+	double e = 10.50;
+	double f = 20.50;
+	double g = e+f;
+	
+	System.out.println(g); 
+		
+	}
+ 
+}
